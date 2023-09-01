88f2b9fb17e42e4b5663d5307bfa7a5631909d14
Justin Russo <j.russo@outlook.com>
2023-09-01 10:29:44 -0400

    fix schema file paths in vscode settings

--- scripts/generator/json-schema-types/schema-files.ts
+++ scripts/generator/json-schema-types/schema-files.ts
@@ -1,16 +1,16 @@
 // A list of schema files to generate types for
 export const schemaFiles = new Map<string, string[]>([
-  ["mc-package.json", ["./mc-package.json"]],
+  ["mc-package.json", ["/mc-package.json"]],
   [
     "minecraft/incontrol/loot.json",
-    ["./src/minecraft/config/incontrol/loot.json"],
+    ["/src/minecraft/config/incontrol/loot.json"],
   ],
   [
     "minecraft/incontrol/spawn.json",
-    ["./src/minecraft/config/incontrol/spawn.json"],
+    ["/src/minecraft/config/incontrol/spawn.json"],
   ],
   [
     "minecraft/incontrol/spawner.json",
-    ["./src/minecraft/config/incontrol/spawner.json"],
+    ["/src/minecraft/config/incontrol/spawner.json"],
   ],
 ]);
