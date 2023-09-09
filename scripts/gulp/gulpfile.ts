86de64ece16840bcea9e2c8d8aa29c2541845113
Justin Russo <j.russo@outlook.com>
2023-09-09 17:36:57 -0400

    improve gulpfile structure

--- scripts/gulp/gulpfile.ts
+++ scripts/gulp/gulpfile.ts
@@ -1,7 +1,5 @@
-import gulp from "gulp";
 import { defaultTask } from "./tasks/default";
-import { start } from "./tasks/start";
+
+export * from "./tasks/start";
 
 module.exports.default = defaultTask;
-
-export { start };
