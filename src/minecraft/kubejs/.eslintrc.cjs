e403f60d542668ac8170459f305d4ad1eec84740
Justin Russo <j.russo@outlook.com>
2023-09-17 21:38:33 -0400

    define kubejs globals

---
+++ src/minecraft/kubejs/.eslintrc.cjs
@@ -0,0 +1,37 @@
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  // globals: {},
+  overrides: [
+    {
+      files: "client_scripts/**/*",
+      globals: {
+        ClientEvents: "readonly",
+        ItemEvents: "readonly",
+        NetworkEvents: "readonly",
+      },
+    },
+    {
+      files: "server_scripts/**/*",
+      globals: {
+        BlockEvents: "readonly",
+        EntityEvents: "readonly",
+        ItemEvents: "readonly",
+        LevelEvents: "readonly",
+        NetworkEvents: "readonly",
+        PlayerEvents: "readonly",
+        ServerEvents: "readonly",
+      },
+    },
+    {
+      files: "startup_scripts/**/*",
+      globals: {
+        BlockEvents: "readonly",
+        ItemEvents: "readonly",
+        StartupEvents: "readonly",
+        WorldgenEvents: "readonly",
+      },
+    },
+  ],
+};
+
+module.exports = config;
