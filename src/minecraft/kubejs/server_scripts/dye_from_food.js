f37264c1d173d8c5808f2d400d040d2850516462
Justin Russo <j.russo@outlook.com>
2023-12-03 12:11:03 -0500

    fix(mc/kubejs): dye from food eating not working

--- src/minecraft/kubejs/server_scripts/dye_from_food.js
+++ src/minecraft/kubejs/server_scripts/dye_from_food.js
@@ -1,18 +1,18 @@
 /**
  * Maps a food ID to a dye ID that is given when the player eats the food.
  *
- * @type {Map<Special.Item, Special.Item>}
+ * @type {Record<string, string>}
  */
-const dyeFoodPairs = new Map([
-  ["sf5_things:blue_apple", "minecraft:blue_dye"],
-  ["minecraft:dirt", "minecraft:brown_dye"],
-  ["sf5_things:green_apple", "minecraft:green_dye"],
-  ["minecraft:carrot", "minecraft:orange_dye"],
-  ["sf5_things:white_apple", "minecraft:white_dye"],
-]);
+const dyeFoodPairs = {
+  "sf5_things:blue_apple": "minecraft:blue_dye",
+  "minecraft:dirt": "minecraft:brown_dye",
+  "sf5_things:green_apple": "minecraft:green_dye",
+  "minecraft:carrot": "minecraft:orange_dye",
+  "sf5_things:white_apple": "minecraft:white_dye",
+};
 
 ItemEvents.foodEaten((event) => {
-  const dye = dyeFoodPairs.get(event.getItem().getId());
+  const dye = dyeFoodPairs[event.getItem().getId()];
 
   if (dye) {
     event.getPlayer().give(dye);
