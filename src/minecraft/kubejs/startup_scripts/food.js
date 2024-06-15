7fb05eb17090cb7158836887efe5409a8d1c9232
Darkosto <darkosto@darkosto.tv>
2024-06-14 23:26:38 -0700

    Allows vanilla dye items to be eaten

--- src/minecraft/kubejs/startup_scripts/food.js
+++ src/minecraft/kubejs/startup_scripts/food.js
@@ -1,11 +1,52 @@
+const groupedItemsToModify = [
+  {
+    ids: [
+      "minecraft:white_dye",
+      "minecraft:orange_dye",
+      "minecraft:magenta_dye",
+      "minecraft:light_blue_dye",
+      "minecraft:yellow_dye",
+      "minecraft:lime_dye",
+      "minecraft:pink_dye",
+      "minecraft:gray_dye",
+      "minecraft:light_gray_dye",
+      "minecraft:cyan_dye",
+      "minecraft:purple_dye",
+      "minecraft:blue_dye",
+      "minecraft:brown_dye",
+      "minecraft:green_dye",
+      "minecraft:red_dye",
+      "minecraft:black_dye",
+    ],
+    hunger: 1,
+    saturation: 0.25,
+    fastToEat: false,
+    alwaysEdible: true,
+  },
+  {
+    ids: ["minecraft:dirt"],
+    hunger: 1,
+    saturation: 1,
+    fastToEat: false,
+    alwaysEdible: true,
+    onEaten: (e) => e.player.tell("you ate"),
+  },
+];
+
 ItemEvents.modification((event) => {
-  event.modify("minecraft:dirt", (item) => {
-    item.foodProperties = (food) => {
-      food.hunger(1);
-      food.saturation(1);
-      food.fastToEat(false);
-      food.eaten((e) => e.player.tell("you ate")); // this is broken, use ItemEvents.foodEaten instead.
-      food.alwaysEdible(true);
-    };
+  groupedItemsToModify.forEach((group) => {
+    group.ids.forEach((id) => {
+      event.modify(id, (itemToModify) => {
+        itemToModify.foodProperties = (food) => {
+          food.hunger(group.hunger);
+          food.saturation(group.saturation);
+          food.fastToEat(group.fastToEat);
+          if (group.onEaten) {
+            food.eaten(group.onEaten); // Note: You mentioned this is broken, consider alternative
+          }
+          food.alwaysEdible(group.alwaysEdible);
+        };
+      });
+    });
   });
 });
