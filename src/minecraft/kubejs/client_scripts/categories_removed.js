1998769dbc44d3f1378ed978621b98326bb0af83
Darkosto <darkosto@darkosto.tv>
2024-08-15 00:44:19 -0700

    REI Category updates

--- src/minecraft/kubejs/client_scripts/categories_removed.js
+++ src/minecraft/kubejs/client_scripts/categories_removed.js
@@ -1,7 +1,6 @@
 REIEvents.removeCategories((event) => {
   console.info(event.getCategoryIds()); //log a list of all category ids to logs/kubejs/client.txt
 
-  //event.remove works too, but yeeting is so much more fun 😉
   event.remove(
     "obtrophies:trophy",
     "exnihilosequentia:dry_sifting",
