6aa5d7223377df622546b135c9a7c9b20098bae7
Darkosto <darkosto@darkosto.tv>
2024-08-20 21:15:03 -0700

    Cleanup: Removes unneeded recipe replacers script

--- src/minecraft/kubejs/server_scripts/recipe_replacers.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.recipes((event) => {
-  // Swap `minecraft:crafting_table` with `forge:workbench` tag
-  event.replaceInput(
-    { input: "minecraft:crafting_table" },
-    "minecraft:crafting_table",
-    "#forge:workbench",
-  );
-});
